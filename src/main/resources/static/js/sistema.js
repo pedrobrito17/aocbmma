6533731d0d1fd72c6b12aa0968ba238d2c606d81
Pedro <pedrobrito17@gmail.com>
2019-04-11 10:46:02 -0300

    sistema responsivo

--- src/main/resources/static/js/sistema.js
+++ src/main/resources/static/js/sistema.js
@@ -1,3 +1,7 @@
+$('.user').click(function () {
+    $('#alterar-foto-perfil').modal();
+});
+
 $('.menu-down').click(function(){
     var display = $(this).find('ul').css('display');
     if(display == 'none'){
@@ -6,8 +10,43 @@ $('.menu-down').click(function(){
     else{
         $(this).find('ul').css('display','none');
     }
+});
+
+$(document).ready(function () {
+    construcaoMenuMobile();
 
 });
 
+//constrói o menu mobile
+function construcaoMenuMobile() {
+    var nav = $('.col-menu');
+    $('#mobileLinks').append(nav.html());
+}
 
+function menuMobile() {
+    var mobileLinks = $('#mobileLinks');
+    if (mobileLinks.css('display') === "block") {
+
+        mobileLinks.css('display', 'none');
+
+    } else {
+
+        mobileLinks.css('display', 'block');
+
+        $('.user').click(function () {
+            $('#alterar-foto-perfil').modal();
+        });
+
+        $('.menu-down').click(function(){
+            var display = $(this).find('ul').css('display');
+            if(display == 'none'){
+                $(this).find('ul').css('display','block');
+            }
+            else{
+                $(this).find('ul').css('display','none');
+            }
+        });    
+        
+    }
+}
 
